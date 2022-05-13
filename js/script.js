4a4711b3400a0b2abb2eda998c325d6d257bb606
aiveris@gmail.com <Staind0323>
2022-05-13 08:22:28 +0300

    2022-05-13

--- js/script.js
+++ js/script.js
@@ -328,7 +328,7 @@ function today() {
   let date2 = new Date();
   let day = date2.getDate();
 
-  if (month == 5) {
+  if (month == 6) {
     day = date2.getDate() + 100;
   }
   // const day = 3;
