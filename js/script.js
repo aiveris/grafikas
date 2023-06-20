9fda67d3c5ed14ea380608bc5dc20baf37044270
Mindaugas <aiveris@gmail.com>
2023-06-20 17:37:53 +0200

    2023-06-20

--- js/script.js
+++ js/script.js
@@ -328,7 +328,7 @@ function today() {
   let date2 = new Date();
   let day = date2.getDate();
 
-  if (month == 5) {
+  if (month == 7) {
     day = date2.getDate() + 100;
   }
   // const day = 3;
