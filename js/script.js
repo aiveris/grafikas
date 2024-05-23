329d848db4db6ed60509ad007f789c004c87a03e
Mindaugas Beleka <aiveris@gmail.com>
2024-05-23 18:57:01 +0300

    2024-05-23

--- js/script.js
+++ js/script.js
@@ -328,7 +328,7 @@ function today() {
   let date2 = new Date();
   let day = date2.getDate();
 
-  if (month == 4) {
+  if (month == 6) {
     day = date2.getDate() + 100;
   }
   // const day = 3;
