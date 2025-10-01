372bd9570775373cef400a458da54368db00ef92
Mindaugas Beleka <50046527+aiveris@users.noreply.github.com>
2025-10-01 10:14:02 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -328,7 +328,7 @@ function today() {
   let date2 = new Date();
   let day = date2.getDate();
 
-  if (month == 10) {
+  if (month == 9) {
     day = date2.getDate() + 100;
   }
   // const day = 3;
